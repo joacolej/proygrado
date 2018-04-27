10c1e7b0e66418b5f2e4f2012c66f8bcb254f9c8
Joaquín Lejtreger <joaclej@gmail.com>
2018-04-27 20:52:08 -0300

    Se eliminan gerundios y correciones de estilo

--- src/ejercicio_verbos.py
+++ src/ejercicio_verbos.py
@@ -1,6 +1,6 @@
 import nltk
-import verbos
-import oraciones
+import verbos as vb
+import oraciones as orac
 from pattern.en import conjugate, lemma, lexeme
 
 def ejercicio_verbos(nombre_texto):
@@ -11,7 +11,7 @@ def ejercicio_verbos(nombre_texto):
 
 def procesar_ejercicio_verbos(texto):
     tokens = nltk.word_tokenize(texto)
-    lista_verbos = verbos.obtener_verbos(tokens)
+    lista_verbos = vb.obtener_verbos(tokens)
     opciones = []
     for idx, verbo in enumerate(lista_verbos):
         conjugaciones = lexeme(verbo['token'])
@@ -22,7 +22,7 @@ def procesar_ejercicio_verbos(texto):
         }
         opciones.append(opcion)
     ejercicio = {
-        'texto': oraciones.sustituir_verbos(tokens, lista_verbos),
+        'texto': orac.sustituir_verbos(tokens, lista_verbos),
         'opciones': opciones
     }
     return ejercicio
