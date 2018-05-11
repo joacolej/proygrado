133ace93223d9b6d0aeac014a5469928804976da
joacolej <joaclej@gmail.com>
2018-05-11 18:54:52 -0300

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,5 +1,3 @@
-#AQUI VA IR EL SERVIDOR :)
-
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
@@ -18,8 +16,6 @@ class Verbos(Resource):
         texto = content.get('texto')
         ret = procesar_ejercicio_verbos(texto)
         return jsonify(ret)
-
-
 
 api.add_resource(Verbos, '/ejercicio-verbos', methods=['POST']) # Route_1
 
