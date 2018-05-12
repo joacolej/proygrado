54c6c2a98995c0cf3a21e43417d72283364f73ee
Joaquín Lejtreger <joaclej@gmail.com>
2018-05-12 19:44:06 -0300

    Un PR que va a marca un antes y un despues

--- src/ejercicio_verbos.py
+++ src/ejercicio_verbos.py
@@ -3,6 +3,7 @@ import verbos as vb
 import oraciones as orac
 from pattern.en import lexeme
 
+
 def ejercicio_verbos(nombre_texto):
     archivo = open(nombre_texto, 'r+')
     data = archivo.read()
@@ -10,20 +11,28 @@ def ejercicio_verbos(nombre_texto):
     return ejercicios
 
 def procesar_ejercicio_verbos(texto):
-    tokens = nltk.word_tokenize(texto)
-    lista_verbos = vb.obtener_verbos(tokens)
-    opciones = []
-    for idx, verbo in enumerate(lista_verbos):
-        conjugaciones = lexeme(verbo['token'])
-        opcion = {
-            'posicion': verbo['posicion'],
-            'variantes': conjugaciones,
-            'solucion': verbo['token'],
-            'referencia': str(idx) + ')'
-        }
-        opciones.append(opcion)
+    parrafos = texto.split('\n')
+    posicion_inicial = 0
+    cant_verbos = 0
+    texto_ejercicio = []
+    for parrafo in parrafos:
+        tokens = nltk.word_tokenize(parrafo)
+        lista_verbos = vb.obtener_verbos(tokens)
+        opciones = []
+        for idx, verbo in enumerate(lista_verbos):
+            conjugaciones = lexeme(verbo['token'])
+            opcion = {
+                'posicion': verbo['posicion'] + posicion_inicial,
+                'variantes': conjugaciones,
+                'solucion': verbo['token'],
+                'referencia': '(' + str(idx + cant_verbos) + ')'
+            }
+            opciones.append(opcion)
+        texto_ejercicio.append(orac.sustituir_verbos(tokens, lista_verbos, cant_verbos))
+        posicion_inicial = posicion_inicial + len(tokens)
+        cant_verbos = cant_verbos + len(lista_verbos)
     ejercicio = {
-        'texto': orac.sustituir_verbos(tokens, lista_verbos),
+        'texto': '\n'.join(texto_ejercicio),
         'opciones': opciones
     }
     return ejercicio
