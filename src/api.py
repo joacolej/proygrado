9855c10fb8ee1bc77cfc1d5abd75b138ab947918
Isabel <isabel@neocoast.com>
2018-10-09 18:30:30 -0300

    DUMMYYYYYYYY

--- src/api.py
+++ src/api.py
@@ -7,6 +7,7 @@ from ejercicio_sustantivos import procesar_ejercicio_sustantivos
 from ejercicio_use_en import procesar_use_en
 from procesamiento import serialize_ojectid
 from ejercicio import Ejercicio
+from utils import abrir_json_file
 import json
 
 app = Flask(__name__)
@@ -34,6 +35,12 @@ class UseOfEnglish(Resource):
         ret = procesar_use_en(texto)
         return jsonify(ret)
 
+class ListaMovers(Resource):
+    def get(self):
+        content = request.json
+        data = abrir_json_file('../recursos/lista_palabras_movers.json')
+        return jsonify(data)
+
 class EjercicioArmado(Resource):
     def post(self):
         ej = Ejercicio()
@@ -57,6 +64,7 @@ api.add_resource(Verbos, '/ejercicio-verbos', methods=['POST']) # Route_1
 api.add_resource(Sustantivos, '/ejercicio-sustantivos', methods=['POST']) # Route_2
 api.add_resource(UseOfEnglish, '/ejercicio-use-of-en', methods=['POST']) # Route_3
 api.add_resource(EjercicioArmado, '/ejercicios', methods=['POST', 'GET']) # Route_4
+api.add_resource(ListaMovers, '/lista-movers', methods=['GET']) # Route_5
 
 if __name__ == '__main__':
     app.run(port='3000', threaded=True, host='0.0.0.0')
